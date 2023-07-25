33bf11d5d28be559d75743799bb9dd5bdfe0b0de
sreea1408 <108213270+sreea1408@users.noreply.github.com>
2023-07-25 16:42:28 +0100

    UI: Add base UI infrastructure
    Using the mantine library an initial UI infrastructure has been
    created. Commits following this would continue to add more to the UI.

--- src-frontend/Calendar.jsx
+++ src-frontend/Calendar.jsx
@@ -17,14 +17,9 @@
  *****************************************************************************/
 
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>,
-);
+function Calendar() {
+  return <>calendar</>;
+}
+
+export default Calendar;
